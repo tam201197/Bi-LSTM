0a646ccbd3d22157546e6d564c9d9b2c282ed632
Tam Truong <60787961+tam201197@users.noreply.github.com>
2025-03-06 10:49:46 +0100

    change from single sentence to multi sentences

--- model.py
+++ model.py
@@ -16,8 +16,7 @@ test_path = "./data/test.conll"
 train_path = "./data/train.conll"
 
 
-def preprocessing_data(data_path):
-    #df = pd.read_csv(data_path, sep="\t", header=None, names=["text", "POS", "Chunk", "label"], quoting=3)
+def preprocessing_data(data_path, multi_sentences=False):
     dataset = {
         "doc": [],
         "labels": []
@@ -38,15 +37,20 @@ def preprocessing_data(data_path):
             labels = []
         else:
             tokens = line.split("\t")
-            if tokens[0] == '.':
-                if texts:
-                    dataset["doc"].append(texts)
-                    dataset["labels"].append(labels)
-                texts = []
-                labels = []
-            else:
+            if multi_sentences: 
                 texts.append(tokens[0])
                 labels.append(tokens[-1])
+            else: 
+                if tokens[0] == '.':
+                    if texts:
+                        dataset["doc"].append(texts)
+                        dataset["labels"].append(labels)
+                    texts = []
+                    labels = []
+                else:
+                    texts.append(tokens[0])
+                    labels.append(tokens[-1])
+            
     #new_df = df.drop(columns=['-X-', '-X-'])
     #new_df.columns = ['text', 'label']
     return dataset
@@ -118,7 +122,7 @@ class BiLSTM(nn.Module):
         return self.fc(bi_lstm_output)
 
 def train(model, num_epochs, loss_function, optimizer,
-              train_dataloader, test_dataloader, device):
+              train_data, test_data, device):
     epoch_loss_logger = []
     epoch_f1_scores = []
     best_f1 = 0
@@ -128,15 +132,15 @@ def train(model, num_epochs, loss_function, optimizer,
         train_loss = []
         model.train()
         print("\t Training progress: \n")
-        for embedding, length, label in tqdm(train_dataloader):
+        for embedding, length, label in tqdm(train_data):
             optimizer.zero_grad()
 
             # Forward pass
             predictions = model(embedding)
 
             # Reshape for loss calculation
-            predictions = predictions.view(-1, predictions.shape[-1])  # (batch_size * seq_length, num_classes)
-            label = label.view(-1)  # (batch_size * seq_length)
+            predictions = predictions.view(-1, predictions.shape[-1])  
+            label = label.view(-1)  
 
             # Compute loss
             loss = loss_function(predictions.to(device=device), label.to(device=device))
@@ -157,11 +161,10 @@ def train(model, num_epochs, loss_function, optimizer,
             predictions = []
             targets = []
 
-            for embedding, length, label in tqdm(dev_dataloader):
+            for embedding, length, label in tqdm(test_data):
                 outputs = model(embedding)
-                test_dataloader = torch.argmax(outputs, dim=2)
-                predictions.extend(test_dataloader.view(-1).tolist())
-                #true_label = torch.argmax(label, dim=2)
+                test_outputs = torch.argmax(outputs, dim=2)
+                predictions.extend(test_outputs.view(-1).tolist())
                 targets.extend(label.view(-1).tolist())
 
 
@@ -190,9 +193,7 @@ def visualize_data(data, title, lerning_rate):
 hidden_size = 100
 num_epochs = 20
 
-dev_dataset = preprocessing_data(dev_path)
-train_dataset = preprocessing_data(train_path)
-test_dataset = preprocessing_data(test_path)
+
 
 embeddings_path = "glove.6B.50d.txt"
 embeddings = read_embeddings(embeddings_path)
@@ -203,15 +204,6 @@ tag = df_nan['label'].unique()
 tag2idx = {value: index for index, value in enumerate(tag)}
 output_size = len(tag)
 
-print("Create dataloader for training")
-train_dataloader = create_dataloader(train_dataset, device, 1)
-
-
-print("Create dataloader for testing")
-test_dataloader = create_dataloader(test_dataset, device, 1)
-
-print("Create dataloader for developing")
-dev_dataloader = create_dataloader(dev_dataset, device, 1)
 
 input_size = embeddings_size 
 
@@ -232,14 +224,26 @@ def model_evaluate(model, eval_dataset):
         train_f1_score = f1_score(predictions, targets, average='macro')
     return train_f1_score
 
-def run(lr):
+def run(lr, multi_sentences=False):
+    dev_dataset = preprocessing_data(dev_path, multi_sentences)
+    train_dataset = preprocessing_data(train_path, multi_sentences)
+    test_dataset = preprocessing_data(test_path, multi_sentences)
+    print("Create dataloader for training")
+    train_dataloader = create_dataloader(train_dataset, device, 1)
+
+    print("Create dataloader for testing")
+    test_dataloader = create_dataloader(test_dataset, device, 1)
+
+    print("Create dataloader for developing")
+    dev_dataloader = create_dataloader(dev_dataset, device, 1)
+
     bi_lstm = BiLSTM(input_size, hidden_size, output_size)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(bi_lstm.parameters(), lr=lr)
     loss_function = criterion.to(device)
     train_losses, f1_scores, model = train(bi_lstm, num_epochs, loss_function, optimizer, train_dataloader, dev_dataloader, device)
     visualize_data(train_losses, "Epoch losses", lr)
-    visualize_data(f1_scores, "F1 scores with",lr)
+    visualize_data(f1_scores, "F1 scores",lr)
     
     print(f"Final macro-averaged F1 after 20 epochs: {model_evaluate(model, test_dataloader)}")
 
@@ -249,18 +253,26 @@ def run(lr):
     model.to(device)
     print(f"Final macro-averaged F1 with early stoping: {model_evaluate(model, test_dataloader)}")
 
-run(0.0003)
+run(0.0007, multi_sentences=True)
 
+#Bi-LSTM with single sentence
 
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0007 : 0.6042290590761805
+    # macro-averaged F1 scores using early stopping with lr 0.0007 : 0.6242508059608585
 
-# macro-averaged F1 scores after trained for 20 epoches with lr 0.0007 : 0.6042290590761805
-# macro-averaged F1 scores using early stopping with lr 0.0007 : 0.6242508059608585
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0001 : 0.6165566758295815
+    # macro-averaged F1 scores using early stopping with lr 0.0001 : 0.6165566758295815
 
-# macro-averaged F1 scores after trained for 20 epoches with lr 0.0001 : 0.6165566758295815
-# macro-averaged F1 scores using early stopping with lr 0.0001 : 0.6165566758295815
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0005 : 0.6132677129993109
+    # macro-averaged F1 scores using early stopping with lr 0.0005 : 0.6300586305443221
 
-# macro-averaged F1 scores after trained for 20 epoches with lr 0.0005 : 0.6132677129993109
-# macro-averaged F1 scores using early stopping with lr 0.0005 : 0.6300586305443221
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0003 : 0.6134956384803433
+    # macro-averaged F1 scores using early stopping with lr 0.0003 : 0.6437442288268493
 
-# macro-averaged F1 scores after trained for 20 epoches with lr 0.0003 : 0.6134956384803433
-# macro-averaged F1 scores using early stopping with lr 0.0003 : 0.6437442288268493
+#Bi-LSTM with multiple sentences
+
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0003 : 0.6160199819234193
+    # macro-averaged F1 scores using early stopping with lr 0.0003 : 0.612160217512418
+
+    # macro-averaged F1 scores after trained for 20 epoches with lr 0.0007 : 0.6292275905489979
+    # macro-averaged F1 scores using early stopping with lr 0.0007 : 0.6297390615811478
